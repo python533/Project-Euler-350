a8112213f601edf626ab94df0f6da6b45e79fe3e
python533 <143389231+python533@users.noreply.github.com>
2024-04-29 14:06:18 +0300

    Project Euler 350

---
+++ Project euler 350/Cozum 350.py
@@ -0,0 +1,48 @@
+
+class b():
+    def __init__(self,b,m):
+        self.b=b
+        self.m=m
+
+    def hesapla(self):
+        self.b=self.b*self.b % self.m
+
+
+
+
+def project(n,N,Mod,L,R,X):
+n=1e6
+N=n+10
+Mod=104060401
+L=1e6
+R=1e12
+X=1e18
+
+
+
+
+def project2(t,e,dvs):
+    t=1
+    while t >> self.b:
+        return t
+
+def main():
+    i=1
+    while i<=n:
+        i++
+
+    a=1
+    while a <= n:
+        ++a
+
+
+    c=2
+    while i*c <=n:
+        (f[i * j]) += MOD -dvs[c] * f[i] % MOD %=MOD
+
+
+
+def project3(fpm,t):
+
+
+
